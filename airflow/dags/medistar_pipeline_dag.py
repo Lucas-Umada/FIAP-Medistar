bd10bbd597c4b57837c6da9a47b59b3364bc0485
iuderasPC <iuderas@outlook.com>
2026-06-06 15:18:13 -0300

    feat: create airflow

---
+++ airflow/dags/medistar_pipeline_dag.py
@@ -0,0 +1,59 @@
+import pendulum
+
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+
+with DAG(
+    dag_id="medistar_pipeline",
+    start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
+    schedule=None,
+    catchup=False,
+    description="Pipeline BDDI do Medistar",
+) as dag:
+
+    inicio = EmptyOperator(
+        task_id="inicio"
+    )
+
+    validar_arquivos = BashOperator(
+        task_id="validar_arquivos",
+        bash_command="""
+        test -f /opt/airflow/data/raw/fonte_1_pacientes_atendimentos.csv &&
+        test -f /opt/airflow/data/raw/fonte_2_comunidades_remotas.csv &&
+        echo "Arquivos de entrada encontrados."
+        """
+    )
+
+    transformar_dados = BashOperator(
+        task_id="transformar_dados",
+        bash_command="""
+        cd /opt/airflow &&
+        python scripts/transform_medistar.py
+        """
+    )
+
+    carregar_oracle = BashOperator(
+        task_id="carregar_oracle",
+        bash_command="""
+        cd /opt/airflow &&
+        python scripts/load_database.py
+        """
+    )
+
+    validar_saida = BashOperator(
+        task_id="validar_saida",
+        bash_command="""
+        echo "Arquivos gerados em data/silver:" &&
+        ls -lh /opt/airflow/data/silver &&
+        echo "Arquivos gerados em data/gold:" &&
+        ls -lh /opt/airflow/data/gold
+        """
+    )
+
+    fim = EmptyOperator(
+        task_id="fim"
+    )
+
+    inicio >> validar_arquivos >> transformar_dados >> carregar_oracle >> validar_saida >> fim
